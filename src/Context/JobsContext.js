c84713a989b5045f7db1ab8d4df72cb6acfc0a05
ahmedfaroUk1oo <ahmedelmahdy33072@gmail.com>
2024-03-29 01:57:37 +0200

    "second"

--- src/Context/JobsContext.js
+++ src/Context/JobsContext.js
@@ -8,12 +8,13 @@ export const GlobalStorage = ({ children }) => {
   const [filter, setFilter] = useState([]);
 
   //this consts is just for alternate URL to fetch in localhost desktop and smartphone (development mode)
-  const url = ["http://192.168.15.120:3000/api/data.json", "http://localhost:3000/api/data.json"];
-  const screen = window.innerWidth;
+  // const url = ["http://192.168.15.120:3000/api/data.json", "http://localhost:3000/api/data.json"];
+  const url2 = `${window.location.origin}/api/data.json`;
+  // const screen = window.innerWidth;
   //---
 
   useEffect(async () => {
-    const response = await fetch(screen <= 375 ? url[0] : url[1]);
+    const response = await fetch(url2);
     const json = await response.json();
     setJobs(json);
     setData(json);
