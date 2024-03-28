f3757345681f367a753dbb6f92135c6fd9206afc
ahmedfaroUk1oo <ahmedelmahdy33072@gmail.com>
2024-03-29 01:36:09 +0200

    "second"

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import ContextProvider, { GlobalStorage } from './Context/JobsContext';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 
 function App() {
   return (
    <>
- <BrowserRouter>
+ <HashRouter>
       <GlobalStorage>
         <Routes>
           <Route path="/*" element={<Home />} />
         </Routes>
       </GlobalStorage>
-    </BrowserRouter>
+    </HashRouter>
   </>
   );
 }
--- src/Context/JobsContext.js
+++ src/Context/JobsContext.js
@@ -7,9 +7,10 @@ export const GlobalStorage = ({ children }) => {
   const [jobs, setJobs] = useState([]);
   const [filter, setFilter] = useState([]);
 
+  //this consts is just for alternate URL to fetch in localhost desktop and smartphone (development mode)
   const url = ["http://192.168.15.120:3000/api/data.json", "http://localhost:3000/api/data.json"];
   const screen = window.innerWidth;
-
+  //---
 
   useEffect(async () => {
     const response = await fetch(screen <= 375 ? url[0] : url[1]);
