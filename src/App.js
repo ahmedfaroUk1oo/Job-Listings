d8bd076ddaf1b81764142fb032ea2e9ef52bdf7c
ahmedfaroUk1oo <ahmedelmahdy33072@gmail.com>
2024-03-29 01:52:55 +0200

    "second"

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React from 'react';
 import './App.css';
 import ContextProvider, { GlobalStorage } from './Context/JobsContext';
 
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, RouterProvider, Routes, createHashRouter } from 'react-router-dom';
 import Home from './Components/Home';
 
 function App() {
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "*",
+      element: <Home />
+    }
+
+  ])
   return (
    <>
- <HashRouter>
+
       <GlobalStorage>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-        </Routes>
+        {/* <Routes>
+          <Route path="*" element={<Home />} />
+          <Route index element={<Home />}></Route>
+        </Routes> */}
+        <RouterProvider router={router} />
       </GlobalStorage>
-    </HashRouter>
+   
   </>
   );
 }
